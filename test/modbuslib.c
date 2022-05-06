361f910d01ce979d1481f1f79ccf443b932ffb3e
sisrsis <51427417+sisrsis@users.noreply.github.com>
2022-05-07 00:12:06 +0430

    اپدیت برنامه

--- test/modbuslib.c
+++ test/modbuslib.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 #include "mod_crc.h"
-void modbus(uint8_t modbus_id,uint8_t * modbus_data,uint8_t modbus_data_len,uint8_t * ragiser,uint8_t *data_tranmit,uint8_t * data_trasmit_lan)
+void modbus(uint8_t modbus_id,uint8_t * modbus_data,uint8_t modbus_data_len,uint16_t * ragiser,uint8_t *data_tranmit,uint8_t * data_trasmit_lan)
 {
  
     void CRC_CHECK(uint8_t * modbus_data_crc,uint8_t modbus_data_len_crc,uint8_t * data_low,uint8_t * data_high)
@@ -22,18 +22,25 @@ void modbus(uint8_t modbus_id,uint8_t * modbus_data,uint8_t modbus_data_len,uint
         case 0x03:
             data_send[0]=modbus_id;
             data_send[1]=mode;
-            data_send[2]=2*modbus_data[5];
+            data_send[2]=(modbus_data[4]*256)+(modbus_data[5])*2;
             int y=3;
-            for(int x=0;x<len;x++,y++)
-            {
+            for(int x=0;x<len;x++,
+            
+            y++)
+            {   
                 data_send[y]=data[x];// هر دو بایت برسی شود 
                 
             }
-            
-            CRC_CHECK(data_send,y,&data_send[y+1],&data_send[y+2]);
             y=y+2;
-        
-            //ragiser_trasmit=data_send;
+            CRC_CHECK(data_send,y,&data_send[y+1],&data_send[y+2]);
+            
+
+
+            for(int x=0;x<y;x++)
+            {
+                data_tranmit[x]=data_send[x];
+                printf(data_send[x]);
+            }
             *data_trasmit_lan=y; 
             break;
         
@@ -45,15 +52,31 @@ void modbus(uint8_t modbus_id,uint8_t * modbus_data,uint8_t modbus_data_len,uint
     {
         uint8_t start_addreas_ragister=(modbus_data[2]*256)+(modbus_data[3]);
         uint8_t Quantity_addres_ragister=(modbus_data[4]*256)+(modbus_data[5]);//error 1 or 125
-        uint8_t data_read[Quantity_addres_ragister];
+        uint8_t data_read[Quantity_addres_ragister*2];
         int y=0;
-        for(int x=start_addreas_ragister;x<(start_addreas_ragister+Quantity_addres_ragister);x++,y++)
+        for(int x=start_addreas_ragister;x<(start_addreas_ragister+Quantity_addres_ragister);x++,y=y++)
         {
-            data_read[y]=ragiser[x];
+            if(ragiser[0] < 255)
+            {
+                data_read[y]=ragiser[x]>>8;
+                printf("1:%d\n",y);
+                data_read[++y]=ragiser[x];
+                printf("2:%d\n",y);    
+               // printf("%X || %X\n",data_read[y-1],data_read[y]);
+                printf("3:%d\n",y);
+            }
+            else
+            {
+                printf("ok\n");
+                data_read[y]=0;
+                data_read[++y]=ragiser[x];
+            }
+
         }
+          
+
+        printf("%d",y);
         trnsmit(0x03,data_read,y);
-        y=y+2;
-        
 
     }
 
